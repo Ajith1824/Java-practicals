bda31752ed6cde4547e323287392612e6e6afa8f
Ajith1824 <119675397+Ajith1824@users.noreply.github.com>
2022-12-03 12:20:48 +0530

    Add files via upload

---
+++ data types/DataTypeDemo.java
@@ -0,0 +1,45 @@
+package basic;
+
+public class DataTypeDemo {
+		public static void main(String[] args) {
+			
+			//boolean example
+			boolean loT=true;
+			System.out.println(loT);
+			
+			//byte example
+			byte range;
+			range=127;
+			System.out.println(range);
+			
+			//short example
+			short range1;
+			range1=-127;
+			System.out.println(range1);
+			
+			
+			//int example
+			int range2=3000000777700;
+			System.out.println(range2);
+			
+			//long example
+			long range3=3000000000l;
+			System.out.println(range3);
+			
+			//double example
+			double range4=88.99;
+			System.out.println(range4);
+			
+			//char example with unicode
+			char letter='\u0041';
+			System.out.println(letter);
+			
+			char letter2=65;
+			System.out.println(letter2);
+			
+			char letter3='A';
+			System.out.println(letter3);
+
+	}
+
+}
